ababf21b4f5e32fdfaba1ab6cd11c815b31127c6
Aleix Maqueda <aleixmaqueda97@gmail.com>
2019-07-19 09:15:01 +0200

    Not finished

--- recipes.js
+++ recipes.js
@@ -1,6 +1,10 @@
+'use strict';
+
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
 const data = require('./data.js');
+const Recipe = require('./models/Recipe.js');
+
+
 
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
@@ -8,3 +12,51 @@ mongoose.connect('mongodb://localhost/recipeApp')
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+
+
+
+  const createOneRecipe = async () => {
+    try {
+      await Recipe.create({
+        title: 'Brocoli con patatas',
+        level: 'UltraPro Chef',
+        ingredients: ['Brocoli', 'Patatas', 'Ajo', 'Sal'],
+        cuisine: 'Veggie',
+        dishType: 'Dish',
+        duration: 30,
+        creator: 'Anna',
+      })
+      console.log(response.title)
+      // .then((recipe) => {
+      //   console.log(recipe)
+      // })
+      // .catch ((error) => {
+      //   console.log(error)
+      // })
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+
+  const addManyRecipes = async (data) => {
+    try {
+      const response = await Recipe.insertMany(data);
+      response.forEach((recipe) => console.log(recipe.title))
+    }
+    
+    catch (error) {
+      console.log(error);
+    }
+  }
+
+  Recipe.insertMany(data)
+    .then((listOfRecipes) => {
+      listOfRecipes.forEach(recipe => console.log(recipe.title))
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+
+    createOneRecipe();
+    addManyRecipes(data);
